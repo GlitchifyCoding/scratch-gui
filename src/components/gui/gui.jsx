fad43d6e8338b6cc68a91713febb508af9afeead
rgantzos <info@gantzos.com>
2022-11-22 21:39:26 -0500

    new import

--- src/components/gui/gui.jsx
+++ src/components/gui/gui.jsx
@@ -10,6 +10,8 @@ import tabStyles from 'react-tabs/style/react-tabs.css';
 import VM from 'scratch-vm';
 import Renderer from 'scratch-render';
 
+import '/src/scratchtools/index.js'
+
 import Blocks from '../../containers/blocks.jsx';
 import CostumeTab from '../../containers/costume-tab.jsx';
 import TargetPane from '../../containers/target-pane.jsx';
@@ -454,43 +456,3 @@ const mapStateToProps = state => ({
 export default injectIntl(connect(
     mapStateToProps
 )(GUIComponent));
-
-
-const observer = new MutationObserver(callback);
-observer.observe(document.querySelector('body'), { attributes: true, childList: true, subtree: true });
-function callback() {
-    if (document.querySelector('[class^="index_app"]')) {
-        observer.disconnect()
-    var vm = document.querySelector('[class^="index_app"]')['_reactRootContainer'].current.child.stateNode.store.getState().scratchGui.vm
-    window.vm = vm
-    function setCloneCountForSprite() {
-     if (
-       document.querySelector("span.scratchtoolsSpriteCloneCounter") !== null
-     ) {
-       document.querySelector(
-         "span.scratchtoolsSpriteCloneCounter"
-       ).textContent = `${(
-         vm.editingTarget.sprite.clones.length - 1
-       ).toString()} clones`;
-     } else {
-       var foundIt = false;
-       document.querySelectorAll("div").forEach(function (el) {
-         if (foundIt === false) {
-           if (el.className.startsWith("sprite-info_row_")) {
-             console.log("foundIt");
-             foundIt = true;
-             var div = document.createElement("div");
-             div.className = "sprite-info_group_14-B_";
-             div.innerHTML = `<label class="label_input-group_2vTky"><span style="font-size: 0.625rem; font-weight: bold;" class="scratchtoolsSpriteCloneCounter">${(
-               vm.editingTarget.sprite.clones.length - 1
-             ).toString()} clones</span></label>`;
-             el.appendChild(div);
-             el.firstChild.lastChild.style.width = "7rem";
-           }
-         }
-       });
-     }
-   }
-   setInterval(setCloneCountForSprite, 300);
-}
-}
--- src/scratchtools/clones.js
+++ src/scratchtools/clones.js
@@ -1,30 +1,38 @@
-var vm = document.querySelector('[class^="index_app"]')['_reactRootContainer'].current.child.stateNode.store.getState().scratchGui.vm
-                function setCloneCountForSprite() {
-    if (
-      document.querySelector("span.scratchtoolsSpriteCloneCounter") !== null
-    ) {
-      document.querySelector(
-        "span.scratchtoolsSpriteCloneCounter"
-      ).textContent = `${(
-        vm.editingTarget.sprite.clones.length - 1
-      ).toString()} clones`;
-    } else {
-      var foundIt = false;
-      document.querySelectorAll("div").forEach(function (el) {
-        if (foundIt === false) {
-          if (el.className.startsWith("sprite-info_row_")) {
-            console.log("foundIt");
-            foundIt = true;
-            var div = document.createElement("div");
-            div.className = "sprite-info_group_14-B_";
-            div.innerHTML = `<label class="label_input-group_2vTky"><span style="font-size: 0.625rem; font-weight: bold;" class="scratchtoolsSpriteCloneCounter">${(
-              vm.editingTarget.sprite.clones.length - 1
-            ).toString()} clones</span></label>`;
-            el.appendChild(div);
-            el.firstChild.lastChild.style.width = "7rem";
-          }
-        }
-      });
-    }
-  }
-  setInterval(setCloneCountForSprite, 300);
+const observer = new MutationObserver(callback);
+observer.observe(document.querySelector('body'), { attributes: true, childList: true, subtree: true });
+function callback() {
+    if (document.querySelector('[class^="index_app"]')) {
+        observer.disconnect()
+    var vm = document.querySelector('[class^="index_app"]')['_reactRootContainer'].current.child.stateNode.store.getState().scratchGui.vm
+    window.vm = vm
+    function setCloneCountForSprite() {
+     if (
+       document.querySelector("span.scratchtoolsSpriteCloneCounter") !== null
+     ) {
+       document.querySelector(
+         "span.scratchtoolsSpriteCloneCounter"
+       ).textContent = `${(
+         vm.editingTarget.sprite.clones.length - 1
+       ).toString()} clones`;
+     } else {
+       var foundIt = false;
+       document.querySelectorAll("div").forEach(function (el) {
+         if (foundIt === false) {
+           if (el.className.startsWith("sprite-info_row_")) {
+             console.log("foundIt");
+             foundIt = true;
+             var div = document.createElement("div");
+             div.className = "sprite-info_group_14-B_";
+             div.innerHTML = `<label class="label_input-group_2vTky"><span style="font-size: 0.625rem; font-weight: bold;" class="scratchtoolsSpriteCloneCounter">${(
+               vm.editingTarget.sprite.clones.length - 1
+             ).toString()} clones</span></label>`;
+             el.appendChild(div);
+             el.firstChild.lastChild.style.width = "7rem";
+           }
+         }
+       });
+     }
+   }
+   setInterval(setCloneCountForSprite, 300);
+}
+}
