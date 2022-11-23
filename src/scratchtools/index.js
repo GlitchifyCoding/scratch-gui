49efe00bba91fbd3b0da2e8187aad377378426e7
rgantzos <info@gantzos.com>
2022-11-22 21:58:05 -0500

    scroll lists

--- src/scratchtools/index.js
+++ src/scratchtools/index.js
@@ -2,3 +2,4 @@ import './api.js'
 
 import './clones.js'
 import './add-last-option.js'
+import './scroll-lists.js'
---
+++ src/scratchtools/scroll-lists.js
@@ -0,0 +1,8 @@
+var style = document.createElement("style");
+style.innerHTML = `
+.monitor_value-inner_3E9Ou {
+  overflow: scroll;
+  text-overflow: clip;
+}
+`;
+document.body.appendChild(style);
